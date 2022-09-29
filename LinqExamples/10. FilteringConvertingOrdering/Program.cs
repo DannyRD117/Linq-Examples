dc3c11e2a0a2c7c9046b74315deab9a137486c13
Daniel Rodriguez <danny.work117@gmail.com>
2022-09-29 12:12:59 -0500

    lambda joining, filtering and converting exercises added

---
+++ LinqExamples/10. FilteringConvertingOrdering/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+object[] mix = { 1, "string", 'd', new List<int>() { 1, 2, 3, 4 }, new List<int>() { 1, 2, 3, 4 }, "dd", 's', "Hello kitty", 1, 2, 3, 4 };
+
+// OFTYPE
+
+var allIntegers = mix.OfType<int>();
+
+Console.WriteLine(string.Join(", ", allIntegers));
+
+//
+
+Console.WriteLine(new String('-', 40));
+
+List<Person> people = new List<Person>()
+{
+    new Buyer() { Age = 20},
+    new Buyer() { Age = 25},
+    new Buyer() { Age = 20},
+    new Supplier() { Age = 22},
+    new Supplier() { Age = 20}
+};
+
+//var suppliers = from p in people
+//                where p is Buyer
+//                let b = p as Buyer
+//                where b.Age == 20
+//                select p;
+
+var suppliers = people.OfType<Buyer>().Where(b => b.Age == 20);
+
+foreach (var item in suppliers)
+{
+    Console.WriteLine(item.GetType().ToString());
+    Console.WriteLine(item.Age);
+}
+
+
+//CONVERT ALL
+
+var buyersToSuppliers = people.OfType<Buyer>().ToList().ConvertAll(b => new Supplier { Age = b.Age });
+
+var buyersToSuppliers2 = from p in people
+                         where p is Buyer
+                         let b = p as Buyer
+                         select new Supplier
+                         {
+                             Age = b.Age
+                         };
+
+List<int> numbers = new List<int>{ 5, 6, 3, 2, 1, 5, 7, 234, 54, 14, 653, 3, 4, 5, 6, 7 };
+
+var stringNumbers = numbers.ConvertAll(n => n.ToString());
+internal class Person
+{
+    public int Age { get; set; }
+}
+internal class Buyer : Person
+{
+   
+}
+internal class Supplier : Person
+{
+
+}
---
+++ LinqExamples/9. LambdaJoining/Program.cs
@@ -0,0 +1,124 @@
+﻿using ClassLibrary;
+
+List<Buyer> buyers = new List<Buyer>()
+{
+    new Buyer() { Name = "Johny", District = "Fantasy District", Age = 22},
+    new Buyer() { Name = "Peter", District = "Scientists District", Age = 40},
+    new Buyer() { Name = "Paul", District = "Fantasy District", Age = 30},
+    new Buyer() { Name = "Maria", District = "Scientists District", Age = 35},
+    new Buyer() { Name = "Joshua", District = "Scientists District", Age = 40},
+    new Buyer() { Name = "Sylvia", District = "Developers District", Age = 22},
+    new Buyer() { Name = "Rebecca", District = "Scientists District", Age = 30},
+    new Buyer() { Name = "Jaime", District = "Developers District", Age = 35},
+    new Buyer() { Name = "Pierce", District = "Fantasy District", Age = 40},
+};
+
+List<Supplier> suppliers = new List<Supplier>()
+{
+    new Supplier() { Name = "Harrison", District = "Fantasy District", Age = 22},
+    new Supplier() { Name = "Charles", District = "Developers District", Age = 40},
+    new Supplier() { Name = "Hailee", District = "Scientists District", Age = 35},
+    new Supplier() { Name = "Taylor", District = "EarthIsFlat District", Age = 30},
+};
+
+// ** JOING **
+
+var innerJoin = suppliers.Join(buyers, s => s.District, b => b.District,
+                              (s, b) => new {
+                                  SupplierName = s.Name,
+                                  BuyerName = b.Name,
+                                  District = b.District
+                              });
+
+foreach (var item in innerJoin)
+{
+    Console.WriteLine($"District: {item.District}, Supplier: {item.SupplierName}, Buyer: {item.BuyerName}");
+}
+
+Console.WriteLine(new String('-', 40));
+
+var compositeJoin = suppliers.Join(buyers,
+    s => new { s.District, s.Age },
+    b => new { b.District, b.Age },
+    (s, b) => new
+    {
+        SupplierName = s.Name,
+        BuyerName = b.Name,
+        District = s.District,
+        Age = s.Age
+    });
+
+foreach (var item in compositeJoin)
+{
+    Console.WriteLine($"District: {item.District}, Age: {item.Age}");
+    Console.WriteLine($"    Supplier: {item.SupplierName}");
+    Console.WriteLine($"    Buyer: {item.BuyerName}");
+}
+
+// ** GROUP JOIN **
+Console.WriteLine(new String('-', 40));
+
+var groupJoin = suppliers.GroupJoin(buyers, s => s.District, b => b.District, 
+    (s, buyersGroup) => new {
+        SuplierName = s.Name,
+        District = s.District,
+        Buyers = buyersGroup
+    });
+
+foreach (var supplier in groupJoin)
+{
+    Console.WriteLine($"Supplier: {supplier.SuplierName}, District: {supplier.District}");
+
+    foreach (var buyer in supplier.Buyers)
+    {
+        Console.WriteLine($"    {buyer.Name}");
+    }
+}
+
+// ** LEFT OUTER JOIN **
+Console.WriteLine(new String('-', 40));
+
+var leftOuterJoingAnon = suppliers.GroupJoin(buyers, s => s.District, b => b.District,
+    (s, buyersGroup) => new {
+        SuplierName = s.Name,
+        District = s.District,
+        Buyers = buyersGroup
+    }).SelectMany(s => s.Buyers.DefaultIfEmpty(),
+        (s,b) => new
+        {
+            s.SuplierName,
+            s.District,
+            BuyerName = b?.Name ?? "No name",
+            BuyersDistict = b?.District ?? "No place"
+        });
+
+foreach (var item in leftOuterJoingAnon)
+{
+    Console.WriteLine($"District: {item.District}");
+    Console.WriteLine($"    Suplier: {item.SuplierName} - Buyer: {item.BuyerName}");
+}
+
+
+Console.WriteLine(new String('-', 40));
+
+var leftOuterJoingType = suppliers.GroupJoin(buyers, s => s.District, b => b.District,
+    (s, buyersGroup) => new
+    {
+        SuplierName = s.Name,
+        District = s.District,
+        Buyers = buyersGroup.DefaultIfEmpty(new Buyer()
+        {
+            Name = "No one",
+            District = "No place"
+        })
+    });
+
+foreach (var supplier in leftOuterJoingType)
+{
+    Console.WriteLine($"Supplier: {supplier.SuplierName}, District: {supplier.District}");
+
+    foreach (var buyer in supplier.Buyers)
+    {
+        Console.WriteLine($"    {buyer.Name}");
+    }
+}
